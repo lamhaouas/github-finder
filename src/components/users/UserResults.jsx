8666bdf9060175eb4f65885ff86d940eec6022a8
Si mohamed <m.lamhaouas@gmail.com>
2022-04-05 11:32:14 -0400

    add user login and avatar

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -2,7 +2,8 @@ import {useEffect, useState} from 'react'
 import UserItem from './UserItem'
 
 function UserResults() {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
+    const [ loading, setLoading] = useState(true)
     useEffect(() =>{
         getUsers ()
     }, [])
@@ -11,12 +12,18 @@ function UserResults() {
       const response = await fetch(`https://api.github.com/users`)
       const data = await response.json()
       setUsers(data)
+      setLoading(false)
      }
-     return <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-         {users.map((user) => (
-            <UserItem key={user.id} user={user}/>
-         ))}
-     </div>
+     if(!loading){
+        return <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+           <UserItem key={user.id} user={user}/>
+        ))}
+    </div>
+     } else {
+         return <h3 className='text-center'>Loading...</h3>
+     }
+     
 
 }
 export default UserResults
